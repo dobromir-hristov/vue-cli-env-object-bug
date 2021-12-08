332dbd22823b7833285a988bfe0e79288fa852a6
Dobromir Hristov <dhristov@apple.com>
2021-12-08 11:24:04 +0200

    chore: add exmaple

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    const IS_FOO = process.env.isFoo;
+    const { isFoo } = process.env;
+    if ('isFoo' in process.env) {
+      console.log('THEREISFOO');
+    }
+    return {
+      isFoo: IS_FOO === isFoo,
+    };
+  },
+};
 </script>
 
 <style>
